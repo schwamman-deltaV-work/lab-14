7580f87d4c9ef2a045711797bee986a21d8dfd92
Jonathon Schwamman <schwamman@msn.com>
2019-07-25 13:48:36 -0500

    Added cart constructor and populateForm function to generate list of items in catalog

--- js/app.js
+++ js/app.js
@@ -4,7 +4,11 @@
 
 // TODO: Create a "Cart" constructor (named Cart) that has item and quantity
 // properties, and builds an array of items as you create instances
+var Cart = function(item, quantity) {
+  this.item = item;
+  this.quantity = quantity;
 
+}
 
 
 // Product Contructor
